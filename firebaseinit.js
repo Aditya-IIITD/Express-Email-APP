bf66ddd5f4e5b9298376ef360650be2829dc0326
Aditya J <aditya19137@iiitd.ac.in>
2024-02-14 14:20:29 +0530

    firebase added

---
+++ firebaseinit.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDyldI6cKWO5h2cfC_r-uk1D3jo_7ZkBPw",
+  authDomain: "email-app-2c57b.firebaseapp.com",
+  projectId: "email-app-2c57b",
+  storageBucket: "email-app-2c57b.appspot.com",
+  messagingSenderId: "227371130710",
+  appId: "1:227371130710:web:c4ff83fc01d0eae91fc0f7",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export default db;
--- src/Controller/form.controller.js
+++ src/Controller/form.controller.js
@@ -1,12 +1,20 @@
+import db from "../../firebaseinit.js";
+import { addDoc, setDoc, doc, collection } from "firebase/firestore";
+
 class FormController {
   getForm(req, res) {
     res.render("form");
   }
 
-  postForm(req, res) {
+  async postForm(req, res) {
+    const { name, email } = req.body;
+    await addDoc(collection(db, "Users"), {
+      name: name,
+      email: email,
+      time: new Date().toString(),
+    });
     res.render("subscribed");
   }
-
   goBack(req, res) {
     res.redirect("/");
   }
